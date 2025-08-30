3ccd4517799df58dd40d7f649ae32046e272c572
SemenTeplov <eburg-severca@mail.ru>
2025-08-30 18:49:51 +0500

    add-friends-likes исправление

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorHandler.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.io.IOException;
-
 @RestControllerAdvice
 public class ErrorHandler {
     @ExceptionHandler
