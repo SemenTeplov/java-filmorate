b6b4cd94e69cea22b47059cf5386afa61e777c5a
SemenTeplov <eburg-severca@mail.ru>
2025-10-04 22:58:28 +0500

    Task 12

--- src/main/java/ru/yandex/practicum/filmorate/dal/film/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/film/FilmRepository.java
@@ -24,6 +24,10 @@ public class FilmRepository {
     private Integer id = 1;
 
     public Film add(Film film) {
+        if (film.getMpa() != null && film.getMpa().getId() > jdbc.queryForList(ru.yandex.practicum.filmorate.dal.rating.Queries.GET_ALL_QUERY).size()) {
+            throw new NotFoundException("Not found");
+        }
+
         film.setId(id++);
 
         String query = String.format(Queries.ADD_QUERY,
